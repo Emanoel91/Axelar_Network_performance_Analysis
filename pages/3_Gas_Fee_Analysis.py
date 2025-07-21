793d80d4f754537ca4f49784b7809813b5785ae2
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-21 13:46:29 +0330

    Create 3_Gas_Fee_Analysis.py

---
+++ pages/3_Gas_Fee_Analysis.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(layout="wide", page_title="Gas Fee Analysis")
+st.title("Gas Fee Analysis")
+
+st.info("این صفحه به زودی با نمودارهای مربوط به تحلیل کارمزدها تکمیل خواهد شد.")
