8bd6d2000680c139747525fad4a61700c1408851
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-21 16:52:07 +0330

    Update 1_Transaction_Analysis.py

--- pages/1_Transaction_Analysis.py
+++ pages/1_Transaction_Analysis.py
@@ -6,17 +6,7 @@ import plotly.express as px
 # --- Wide Layout ---
 st.set_page_config(layout="wide")
 
-st.title("Axelar Network Performance Analysis")
-
-st.markdown("""
-### About Axelar
-Axelar Network is a decentralized blockchain platform designed to enable seamless interoperability between disparate blockchain ecosystems. 
-Launched to address the fragmentation in the blockchain space, Axelar provides a robust infrastructure for cross-chain communication, 
-allowing different blockchains to securely share data and transfer assets. By leveraging a decentralized network of validators and 
-a universal protocol, Axelar facilitates scalable, secure, and efficient interactions across blockchains, empowering developers to 
-build applications that operate across multiple chains without complex integrations. With its focus on simplifying cross-chain 
-connectivity, Axelar aims to drive the adoption of Web3 by creating a unified, interoperable blockchain environment.
-""")
+st.title("Axelar Network: Transaction Analysis")
 
 # --- Snowflake Connection ---
 conn = snowflake.connector.connect(
