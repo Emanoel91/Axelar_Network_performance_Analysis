8babedc38bdd9f118e8ddbbf26009f4cde775230
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-21 13:46:51 +0330

    Create 4_Block_Analysis.py

---
+++ pages/4_Block_Analysis.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(layout="wide", page_title="Block Analysis")
+st.title("Block Analysis")
+
+st.info("این صفحه به زودی با نمودارهای مربوط به تحلیل بلاک‌ها تکمیل خواهد شد.")
