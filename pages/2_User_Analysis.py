05611786eacd1cb7bde892e72ebba8cb7c1afc90
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-21 13:46:01 +0330

    Create 2_User_Analysis.py

---
+++ pages/2_User_Analysis.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(layout="wide", page_title="User Analysis")
+st.title("User Analysis")
+
+st.info("این صفحه به زودی با نمودارهای مربوط به تحلیل کاربران تکمیل خواهد شد.")
