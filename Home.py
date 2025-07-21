2e72dd4dfea584a6573a82a20648aab133b9304b
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-21 16:52:52 +0330

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,7 +3,13 @@ import streamlit as st
 st.set_page_config(layout="wide", page_title="Axelar Network Performance Analysis")
 
 st.title("Axelar Network Dashboard")
+
 st.markdown("""
-به داشبورد Axelar خوش آمدید.
-از منوی سمت چپ صفحه، بخش مورد نظر خود را انتخاب کنید.
+### About Axelar
+Axelar Network is a decentralized blockchain platform designed to enable seamless interoperability between disparate blockchain ecosystems. 
+Launched to address the fragmentation in the blockchain space, Axelar provides a robust infrastructure for cross-chain communication, 
+allowing different blockchains to securely share data and transfer assets. By leveraging a decentralized network of validators and 
+a universal protocol, Axelar facilitates scalable, secure, and efficient interactions across blockchains, empowering developers to 
+build applications that operate across multiple chains without complex integrations. With its focus on simplifying cross-chain 
+connectivity, Axelar aims to drive the adoption of Web3 by creating a unified, interoperable blockchain environment.
 """)
