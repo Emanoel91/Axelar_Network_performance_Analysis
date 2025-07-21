d8a6b67ca990b24bba18f1e143449d00e923ab2d
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-21 16:56:04 +0330

    Update Home.py

--- Home.py
+++ Home.py
@@ -12,7 +12,9 @@ allowing different blockchains to securely share data and transfer assets. By le
 a universal protocol, Axelar facilitates scalable, secure, and efficient interactions across blockchains, empowering developers to 
 build applications that operate across multiple chains without complex integrations. With its focus on simplifying cross-chain 
 connectivity, Axelar aims to drive the adoption of Web3 by creating a unified, interoperable blockchain environment.
+
+🔵 X: https://x.com/axelar
 """)
 
 
-🔵 X: https://x.com/axelar
+
