1a94ff904b3c9fd05519fb8b98fb8f0e4a97113b
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-22 22:58:39 +0330

    Update 2_User_Analysis.py

--- pages/2_User_Analysis.py
+++ pages/2_User_Analysis.py
@@ -574,6 +574,29 @@ st.dataframe(failed_txns_df.style.format({
 }).highlight_max(subset=["False Txns Count"], color='tomato'))
 
 # --- Row 10: Side-by-Side Charts ---
+
+new_users_df['Year'] = new_users_df['Date'].dt.year.astype(str)
+
+import plotly.express as px
+
+fig_year = px.bar(new_users_df.groupby('Year')['New Users'].sum().reset_index(),
+                  x='Year', y='New Users',
+                  title='New Users Per Year',
+                  labels={'Year':'Year', 'New Users':'Number of New Users'},
+                  text='New Users')
+
+fig_year.update_traces(textposition='outside')
+
+fig_quarter = px.bar(new_users_df,
+                     x='Year', y='New Users',
+                     color='Quarter',
+                     barmode='group',
+                     title='New Users by Quarter per Year',
+                     labels={'Year':'Year', 'New Users':'Number of New Users', 'Quarter':'Quarter'},
+                     text='New Users')
+
+fig_quarter.update_traces(textposition='outside')
+
 col1, col2 = st.columns(2)
 
 with col1:
