f4988cbb047a4de24664a1d864cab4d4aacaaff3
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-23 03:22:34 +0330

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,7 +5,6 @@ st.set_page_config(layout="wide", page_title="Axelar Network Performance Analysi
 st.title("📚Axelar Network Performance Analysis")
 
 st.markdown("""
-##### About Axelar
 Axelar Network is a decentralized blockchain platform designed to enable seamless interoperability between disparate blockchain ecosystems. 
 Launched to address the fragmentation in the blockchain space, Axelar provides a robust infrastructure for cross-chain communication, 
 allowing different blockchains to securely share data and transfer assets. By leveraging a decentralized network of validators and 
