e3bcb539200dba6ae7a431c02356dc845fb61bc8
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-23 03:12:49 +0330

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,8 +3,6 @@ import streamlit as st
 st.set_page_config(layout="wide", page_title="Axelar Network Performance Analysis")
 
 st.title("Axelar Network Dashboard")
-# --- Display Image ---
-st.image("https://i.postimg.cc/J4scdKHx/axelar.jpg", caption="", use_container_width=True)
 
 st.markdown("""
 ### About Axelar
@@ -14,6 +12,9 @@ allowing different blockchains to securely share data and transfer assets. By le
 a universal protocol, Axelar facilitates scalable, secure, and efficient interactions across blockchains, empowering developers to 
 build applications that operate across multiple chains without complex integrations. With its focus on simplifying cross-chain 
 connectivity, Axelar aims to drive the adoption of Web3 by creating a unified, interoperable blockchain environment.
+
+# --- Display Image ---
+st.image("https://i.postimg.cc/Y9yL6KRH/axelar.jpg", caption="", use_container_width=True)
 
 🟠 Website: https://www.axelar.network/
 
