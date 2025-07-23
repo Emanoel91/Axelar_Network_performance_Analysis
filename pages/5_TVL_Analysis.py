008f962d80a32156bf7885f7194793a854271bc2
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-23 17:07:14 +0330

    Update 5_TVL_Analysis.py

--- pages/5_TVL_Analysis.py
+++ pages/5_TVL_Analysis.py
@@ -8,8 +8,8 @@ st.set_page_config(layout="wide")
 
 st.title("Axelar Network: TVL Analysis💸")
 
-# --- گرفتن داده‌ها از Dune API ---
-@st.cache_data(ttl=3600)  # کش به مدت ۱ ساعت
+# --- Getting D.API ---
+@st.cache_data(ttl=3600)  # --- cache for 1 hour
 def load_dune_tvl():
     url = "https://api.dune.com/api/v1/query/5524904/results?api_key=kmCBMTxWKBxn6CVgCXhwDvcFL1fBp6rO"
     response = requests.get(url)
@@ -28,19 +28,19 @@ def load_dune_tvl():
 dune_tvl = load_dune_tvl()
 
 if not dune_tvl.empty:
-    # --- فیلتر جستجوی زنجیره ---
+    # --- chain search filter ---
     chain_list = dune_tvl["Chain"].unique().tolist()
     selected_chain = st.selectbox("🔎 Choose your desired chain", chain_list, index=chain_list.index("Axelar") if "Axelar" in chain_list else 0)
 
-    # --- TVL زنجیره انتخابی ---
+    # --- TVL for selected chain ---
     selected_tvl = dune_tvl.loc[dune_tvl["Chain"] == selected_chain, "TVL"].sum()
     st.metric(label=f"TVL of {selected_chain}", value=f"${selected_tvl:,.0f}")
 
-    # --- جدول ---
+    # --- table ---
     st.markdown("<h4 style='font-size:18px;'>TVL of Different Chains</h4>", unsafe_allow_html=True)
     st.dataframe(dune_tvl.style.format({"TVL": "{:,.0f}"}), use_container_width=True)
 
-    # --- نمودار میله‌ای ---
+    # --- chart ---
     def human_format(num):
         if num >= 1e9:
             return f"{num/1e9:.1f}B"
