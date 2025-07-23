c533262eee70a1c94aeef04f72a9c7b620a4fb31
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-23 17:01:21 +0330

    Update 5_TVL_Analysis.py

--- pages/5_TVL_Analysis.py
+++ pages/5_TVL_Analysis.py
@@ -8,8 +8,8 @@ st.set_page_config(layout="wide")
 
 st.title("Axelar Network: TVL Analysis💸")
 
-# --- گرفتن داده‌ها از Dune API ---
-@st.cache_data(ttl=3600)  # کش به مدت ۱ ساعت
+# --- D.API Functions ---------------------------------------------------------------------------------
+@st.cache_data(ttl=3600)  # cache for 1 hour -------------
 def load_dune_tvl():
     url = "https://api.dune.com/api/v1/query/5524904/results?api_key=kmCBMTxWKBxn6CVgCXhwDvcFL1fBp6rO"
     response = requests.get(url)
@@ -24,23 +24,24 @@ def load_dune_tvl():
         st.error(f"Failed to fetch Dune data: {response.status_code}")
         return pd.DataFrame(columns=["Chain", "Token Symbol", "TVL"])
 
-# --- Load Data ---
+# --- Load Data ---------------------------------------------------------------------------------------------------------
 dune_tvl = load_dune_tvl()
 
+-------------------------------------------------------------------------------------------------------------------------
 if not dune_tvl.empty:
-    # --- فیلتر جستجوی زنجیره ---
+    # --- chain search filter ---------------------------------------------
     chain_list = dune_tvl["Chain"].unique().tolist()
-    selected_chain = st.selectbox("🔎 جستجوی زنجیره", chain_list, index=chain_list.index("Axelar") if "Axelar" in chain_list else 0)
+    selected_chain = st.selectbox("🔎 Choose your desired chain:", chain_list, index=chain_list.index("Axelar") if "Axelar" in chain_list else 0)
 
-    # --- TVL زنجیره انتخابی ---
+    # --- TVL: selected chain ---
     selected_tvl = dune_tvl.loc[dune_tvl["Chain"] == selected_chain, "TVL"].sum()
     st.metric(label=f"TVL of {selected_chain}", value=f"${selected_tvl:,.0f}")
 
-    # --- جدول ---
+    # --- table ---
     st.markdown("<h4 style='font-size:18px;'>TVL of Different Chains</h4>", unsafe_allow_html=True)
     st.dataframe(dune_tvl.style.format({"TVL": "{:,.0f}"}), use_container_width=True)
 
-    # --- نمودار میله‌ای ---
+    # --- chart ---
     def human_format(num):
         if num >= 1e9:
             return f"{num/1e9:.1f}B"
@@ -56,11 +57,11 @@ if not dune_tvl.empty:
         x="Chain",
         y="TVL",
         color="Chain",
-        title="Top Chains by TVL",
+        title="🏆Top Chains by TVL",
         text=dune_tvl.head(15)["TVL"].apply(human_format)
     )
     fig.update_traces(textposition="outside")
-    fig.update_layout(xaxis_title="Chain", yaxis_title="TVL", showlegend=False)
+    fig.update_layout(xaxis_title="", yaxis_title="$USD", showlegend=False)
     st.plotly_chart(fig, use_container_width=True)
 else:
     st.warning("No data available.")
