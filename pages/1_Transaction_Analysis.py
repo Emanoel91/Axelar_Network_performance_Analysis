44391cf7b78072f39659ba49b03c9ec3b6327cd5
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-08-11 03:11:58 +0330

    Update 1_Transaction_Analysis.py

--- pages/1_Transaction_Analysis.py
+++ pages/1_Transaction_Analysis.py
@@ -15,16 +15,54 @@ st.set_page_config(layout="wide")
 
 st.title("Axelar Network: Transaction Analysis⛓")
 
-st.info(
-    "📊Charts initially display data for a default time range. Select a custom range to view results for your desired period."
+st.info("📊Charts initially display data for a default time range. Select a custom range to view results for your desired period.")
+st.info("⏳On-chain data retrieval may take a few moments. Please wait while the results load.")
 
+# --- Sidebar Footer Slightly Left-Aligned ---------------------------------------------------------------------------------------------------------
+st.sidebar.markdown(
+    """
+    <style>
+    .sidebar-footer {
+        position: fixed;
+        bottom: 20px;
+        width: 250px;
+        font-size: 13px;
+        color: gray;
+        margin-left: 5px; # -- MOVE LEFT
+        text-align: left;  
+    }
+    .sidebar-footer img {
+        width: 16px;
+        height: 16px;
+        vertical-align: middle;
+        border-radius: 50%;
+        margin-right: 5px;
+    }
+    .sidebar-footer a {
+        color: gray;
+        text-decoration: none;
+    }
+    </style>
+
+    <div class="sidebar-footer">
+        <div>
+            <a href="https://x.com/axelar" target="_blank">
+                <img src="https://img.cryptorank.io/coins/axelar1663924228506.png" alt="Axelar Logo">
+                Powered by Axelar
+            </a>
+        </div>
+        <div style="margin-top: 5px;">
+            <a href="https://x.com/0xeman_raz" target="_blank">
+                <img src="https://pbs.twimg.com/profile_images/1841479747332608000/bindDGZQ_400x400.jpg" alt="Eman Raz">
+                Built by Eman Raz
+            </a>
+        </div>
+    </div>
+    """,
+    unsafe_allow_html=True
 )
 
-st.info(
-    "⏳On-chain data retrieval may take a few moments. Please wait while the results load."
-)
-
-# --- Snowflake Connection ---
+# --- Snowflake Connection -------------------------------------------------------------------------------------------------------------------------
 conn = snowflake.connector.connect(
     user=st.secrets["snowflake"]["user"],
     password=st.secrets["snowflake"]["password"],
@@ -36,7 +74,7 @@ conn = snowflake.connector.connect(
 
 # --- Time Frame & Period Selection ---
 timeframe = st.selectbox("Select Time Frame", ["month", "week", "day"])
-start_date = st.date_input("Start Date", value=pd.to_datetime("2025-01-01"))
+start_date = st.date_input("Start Date", value=pd.to_datetime("2023-01-01"))
 end_date = st.date_input("End Date", value=pd.to_datetime("2025-07-31"))
 
 # --- Query Functions ---
